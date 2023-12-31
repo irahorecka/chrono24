095512a1dc7fbc31cbffe4787ccf7d6626861739
Ira Horecka <ira89@icloud.com>
2023-12-31 13:40:14 -0800

    update module call class name and docstrings

--- chrono24/__init__.py
+++ chrono24/__init__.py
@@ -9,11 +9,21 @@ from chrono24.api import chrono24
 from chrono24.exceptions import NoListingsFoundException, RequestException
 
 
-class module_call:
+class Chrono24ModuleCall:
+    """A class designed to provide a callable interface for accessing the chrono24 module."""
+
     def __call__(self, query):
+        """Call method to execute the Chrono24ModuleCall class instance as a function.
+
+        Args:
+            query (str): A string representing the query to be passed to chrono24.
+
+        Returns:
+            chrono24: An object or result returned by the chrono24 instance.
+        """
         return chrono24(query)
 
 
-sys.modules[__name__] = module_call()
+sys.modules[__name__] = Chrono24ModuleCall()
 
 __version__ = "0.0.1"
