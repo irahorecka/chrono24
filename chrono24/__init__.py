a61bd5d86521d40411b823a2982b8d566ec85339
Ira Horecka <ira89@icloud.com>
2023-12-30 21:00:07 -0800

    update chrono24/__init__.py

--- chrono24/__init__.py
+++ chrono24/__init__.py
@@ -3,4 +3,16 @@ chrono24
 ~~~~~~~~
 """
 
+import sys
+
+from chrono24.api import chrono24
+
+
+class module_call:
+    def __call__(self, query):
+        return chrono24(query)
+
+
+sys.modules[__name__] = module_call()
+
 __version__ = "0.0.1"
