913d36339e071f356e23c8dd2296f711b459c51c
Ira Horecka <ira89@icloud.com>
2024-09-30 13:10:51 -0400

    update version from 0.2.13 --> 0.3.0

--- chrono24/__init__.py
+++ chrono24/__init__.py
@@ -5,5 +5,6 @@ chrono24
 
 # Import chrono24 and use chrono24.query to interface Chrono24
 from chrono24.api import Chrono24 as query
+from chrono24.filters import Filters as filters
 
-__version__ = "0.2.13"
+__version__ = "0.3.0"
